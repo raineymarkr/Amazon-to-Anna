2f47f4d690604eb1f8599048f1b74b20f9870eb6
raineymarkr <97651651+raineymarkr@users.noreply.github.com>
2023-10-18 06:24:49 -0500

    Update background.js
    Added international support for Amazon URLS

--- background.js
+++ background.js
@@ -3,7 +3,21 @@ browser.contextMenus.create({
   id: "run-script",
   title: "Find on Anna's Archive",
   contexts: ["page"],
-  documentUrlPatterns: ["https://*.amazon.com/*"]
+  documentUrlPatterns: ["https://*.amazon.com/*",
+	"https://*.amazon.ca/*",
+	"https://*.amazon.cn/*",
+	"https://*.amazon.co.jp/*",
+	"https://*.amazon.com.au/*",
+	"https://*.amazon.com.mx/*",
+	"https://*.amazon.co.uk/*",
+	"https://*.amazon.de/*",
+	"https://*.amazon.es/*",
+	"https://*.amazon.eu/*",
+	"https://*.amazon.fr/*",
+	"https://*.amazon.in/*",
+	"https://*.amazon.it/*",
+	"https://*.amazon.nl/*",
+	"https://*.amazon.sa/*"]
 });
 
 // Listen for context menu click
